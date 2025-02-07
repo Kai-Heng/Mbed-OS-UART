e1cf31f77e98c5cc783064db798957d88f1bf406
Kai-Heng <gankaiheng@gmail.com>
2025-02-06 19:15:50 -0600

    transmit and receive data between PC and STM32 Nucleo

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*UART demo. Sends message to PC terminal
+ */
+                                             
+#include "mbed.h"
+BufferedSerial pc(USBTX, USBRX,9600);     
+int cycle=0;
+
+int main() {
+  while(true){
+    printf("Hello World. This is loop %i \n\r",cycle);  
+    thread_sleep_for(500);
+    cycle++;
+  }      
+}
